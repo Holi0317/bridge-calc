c0a4f8f73d27bfee38ad7b5066e9f6903f166c66
Holi0317 <holliswuhollis@gmail.com>
2016-06-30 20:45:03 +0800

    Revert "FEAT: Deploy to Github page script"
    This reverts commit 2667d52bd62cafaa36844110f14fcadb11a1804c.

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let gulp = require('gulp');
-let ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy', () => {
-  return gulp.src('build/bundled/**/*')
-    .pipe(ghPages());
-})
