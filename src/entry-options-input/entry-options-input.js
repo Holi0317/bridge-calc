425ce833a96357f8913f579b21c62847526d1432
holi0317 <holliswuhollis@gmail.com>
2017-01-12 10:59:40 +0800

    Use ref for referring element

--- src/entry-options-input/entry-options-input.js
+++ src/entry-options-input/entry-options-input.js
@@ -1,24 +1,13 @@
-import {inject, LogManager, bindable, bindingMode} from 'aurelia-framework';
+import {LogManager, bindable, bindingMode} from 'aurelia-framework';
 
 const logger = LogManager.getLogger('EntryOptionsInputComponent');
 
-@inject(Element)
 export class EntryOptionsInput {
-  el: HTMLElement;
-  inputEl: ?HTMLInputElement;
+  inputEl: HTMLInputElement;
   @bindable({ defaultBindingMode: bindingMode.twoWay }) value: string;
   @bindable() error: string;
   @bindable() label: string;
   @bindable() type: string;
-
-  constructor(el: HTMLElement) {
-    this.el = el;
-    this.inputEl = null;
-  }
-
-  attached() {
-    this.inputEl = this.el.querySelector('input');
-  }
 
   errorChanged(newValue: string, oldValue: string) {
     if (!this.inputEl) {
