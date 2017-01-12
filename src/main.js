0f2453c2b30b539fcb88baedbfa8567f6deba2d1
holi0317 <holliswuhollis@gmail.com>
2017-01-12 10:50:59 +0800

    Use import() statement

--- src/main.js
+++ src/main.js
@@ -17,12 +17,11 @@ export async function configure(aurelia: Aurelia) {
   // aurelia.use.plugin('aurelia-animator-css');
   // if the css animator is enabled, add swap-order="after" to all router-view elements
 
-  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  // aurelia.use.plugin('aurelia-html-import-template-loader')
-
   // Conditionally Load in fetch polyfill
   if (!self.fetch) {
     await System.import('whatwg-fetch');
+    // Idea fails at parsing new import() statement.
+    // await import('whatwg-fetch');
   }
 
   await aurelia.start();
@@ -31,7 +30,7 @@ export async function configure(aurelia: Aurelia) {
   // if you would like your website to work offline (Service Worker), 
   // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
   /*
-  const offline = await System.import('offline-plugin/runtime');
+  const offline = await import('offline-plugin/runtime');
   offline.install();
   */
 }
