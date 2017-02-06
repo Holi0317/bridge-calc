e6b6d07bcea385979f45713d8cf2c6bff0fcdb46
holi0317 <holliswuhollis@gmail.com>
2017-02-06 19:49:01 +0800

    Allow disabled to bind in mdl-input

--- src/mdl-input/mdl-input.ts
+++ src/mdl-input/mdl-input.ts
@@ -1,23 +1,40 @@
-import {bindable, bindingMode} from 'aurelia-framework';
+import {bindable, bindingMode, inject, TaskQueue} from 'aurelia-framework';
 import {getLogger} from 'aurelia-logging';
 
 const logger = getLogger('MdlInputComponent');
 
+@inject(TaskQueue)
 export class MdlInput {
   @bindable({ defaultBindingMode: bindingMode.twoWay }) public value: string;
   @bindable() public error: string;
   @bindable() public label: string;
   @bindable() public type: string;
+  @bindable() public disabled = false;
   private _inputEl: HTMLInputElement;
+  // _el type should be HTMLElement. MDL has extended it so TS cannot check its type.
+  private _el: any;
+
+  constructor(private _taskQueue: TaskQueue) {
+
+  }
 
   errorChanged(newValue: string, oldValue: string) {
     if (!this._inputEl) {
       return
     }
     this._inputEl.setCustomValidity(newValue);
+    this._taskQueue.queueMicroTask(() => {
+      if (this._el.MaterialTextfield) {
+        this._el.MaterialTextfield.updateClasses_();
+      }
+    })
+  }
 
-    // Fire input event to force MDL to check validity
-    const event = new Event('input', {bubbles: true});
-    this._inputEl.dispatchEvent(event);
+  disabledChanged() {
+    this._taskQueue.queueMicroTask(() => {
+      if (this._el && this._el.MaterialTextfield) {
+        this._el.MaterialTextfield.checkDisabled();
+      }
+    });
   }
 }
