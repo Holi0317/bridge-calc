33761e4ccdebaba76ed18455918fa31d75bdc17a
holi0317 <holliswuhollis@gmail.com>
2017-04-04 00:19:11 +0800

    Copy more files to dist folder on build

--- webpack-cfg/copy-files.js
+++ webpack-cfg/copy-files.js
@@ -3,7 +3,8 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   plugins: [
     new CopyWebpackPlugin([
-      {from: 'favicon.ico', to: 'favicon.ico'}
+      {from: 'favicon.ico', to: 'favicon.ico'},
+      {from: 'manifest.json', to: 'manifest.json'}
     ])
   ]
 }
--- webpack-cfg/gen-index.js
+++ webpack-cfg/gen-index.js
@@ -1,16 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {ENV} = require('./paths')
 
+const htmlSettings = {
+  template: 'index.ejs',
+  chunksSortMode: 'dependency',
+  minify: (ENV === 'production') ? {
+    removeComments: true,
+    collapseWhitespace: true
+  } : undefined,
+  baseUrl: '/'
+}
+
 module.exports = {
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.ejs',
-      chunksSortMode: 'dependency',
-      minify: (ENV === 'production') ? {
-        removeComments: true,
-        collapseWhitespace: true
-      } : undefined,
-      baseUrl: '/'
-    }),
+    new HtmlWebpackPlugin(htmlSettings),
+    new HtmlWebpackPlugin(Object.assign({}, htmlSettings, {
+      filename: '404.html'
+    }))
   ]
 }
