a80989763cfd80a2210be276d1e8530c8056ccdc
holi0317 <holliswuhollis@gmail.com>
2017-04-04 23:11:34 +0800

    Set theme for mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {createElement as h} from 'react'
 import {render} from 'react-dom'
+import {muiTheme} from './mui-theme'
 
 function App() {
-  return <MuiThemeProvider>
+  return <MuiThemeProvider muiTheme={muiTheme}>
     <span>Hello World</span>
   </MuiThemeProvider>
 }
