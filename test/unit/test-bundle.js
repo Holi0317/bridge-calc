12e140ce3f2e137339acd711de009509a0d7c62f
holi0317 <holliswuhollis@gmail.com>
2017-04-04 23:03:46 +0800

    Disable test for now

--- test/unit/test-bundle.js
+++ test/unit/test-bundle.js
@@ -1,13 +1,4 @@
 // require all modules ending in "_test" from the
 // current directory and all subdirectories
-
-// Disable tests during migration
-
-// const testsContext = require.context('.', true, /\.spec$/)
-// testsContext.keys().forEach(testsContext)
-
-import test from 'tape'
-test('placeholder', t => {
-  t.pass('Test is disabled for now')
-  t.end()
-})
+const testsContext = require.context('.', true, /\.spec$/)
+testsContext.keys().forEach(testsContext)
