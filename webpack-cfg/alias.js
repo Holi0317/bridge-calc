d1952a44cd14ae8f2bc09521f7143010d591b438
holi0317 <holliswuhollis@gmail.com>
2017-04-11 14:33:34 +0800

    Remove propTypes in production
    Some react library have other place that use proptypes library.
    We cannot null the proptypes library until they can be removed by babel plugin.

--- webpack-cfg/alias.js
+++ webpack-cfg/alias.js
@@ -1,8 +1,15 @@
 module.exports = {
   resolve: {
     alias: {
+      // Use preact instead of react
       'react': 'preact-compat',
       'react-dom': 'preact-compat',
+
+      // Use non-compiled version of the following react libraries
+      'react-icon-base': 'react-icon-base/index.js',
+      'react-css-themr': 'react-css-themr/src/index.js',
+
+      // es6 version of localforage
       localforage: 'localforage/src/localforage',
       'localforage-memoryStorageDriver': 'localforage-memoryStorageDriver/dist/localforage-memoryStorageDriver.es6.js'
     }
--- webpack-cfg/babel.js
+++ webpack-cfg/babel.js
@@ -5,7 +5,7 @@ module.exports = {
   module: {
     rules: [{
       test: /\.jsx?$/,
-      exclude: /node_modules(?!\/react-(icons|toolbox)\/)/,
+      exclude: /node_modules(?!\/react-.+\/)/,
       use: {
         loader: 'babel-loader',
         options: {
