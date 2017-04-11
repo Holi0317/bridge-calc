b5a677eec06fb5432127c55d1008ea604f825f04
holi0317 <holliswuhollis@gmail.com>
2017-04-11 17:24:02 +0800

    Basic layout for entry view

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,5 +1,38 @@
-import {h} from 'preact'
+import {h, Component} from 'preact'
+import {Button} from 'react-toolbox/components/button'
+import Collapse from 'react-collapse'
+import {bind} from 'decko'
+import style from './entry.css'
 
-export function Entry() {
-  return <span>Hey</span>
+export class Entry extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      optionsOpened: false
+    }
+  }
+
+  @bind
+  toggleCollapse() {
+    this.setState(state => ({
+      optionsOpened: !state.optionsOpened
+    }))
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h3>Player Names</h3>
+        <div>name-input-list</div>
+        <hr />
+
+        <Button onMouseUp={this.toggleCollapse} label="Options" raised className={style.optionsBtn} />
+        <Collapse isOpened={this.state.optionsOpened}>
+          Entry-options
+        </Collapse>
+
+        <Button label="Start" raised accent className={style.startBtn} />
+      </div>
+    )
+  }
 }
