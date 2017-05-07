b349f66a701a3a65b6acfe4694e0a2bf9600e3f9
holi0317 <holliswuhollis@gmail.com>
2017-05-07 18:39:45 +0800

    Use ES6+ version of react libraries

--- webpack-cfg/alias.js
+++ webpack-cfg/alias.js
@@ -7,7 +7,10 @@ module.exports = {
 
       // Use non-compiled version of the following react libraries
       'react-icon-base': 'react-icon-base/index.js',
-      'react-css-themr': 'react-css-themr/src/index.js'
+      'react-css-themr': 'react-css-themr/src/index.js',
+      'react-collapse': 'react-collapse/src/index.js',
+      'react-height': 'react-height/src/index.js',
+      'react-i18next': 'react-i18next/src/index.js'
     }
   }
 }
