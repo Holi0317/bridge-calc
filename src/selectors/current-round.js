c9bc96b52e9d0a3ccc719a361714f1bf42e2e771
holi0317 <holliswuhollis@gmail.com>
2017-05-09 21:47:13 +0800

    Add current round selector

---
+++ src/selectors/current-round.js
@@ -0,0 +1,16 @@
+// @flow
+import {GameStage} from '../game-stage'
+
+import type {RootState} from '../types'
+
+/**
+ * Selects currentRound property from current game.
+ * If the property does not exists, null will be returned
+ */
+export function currentRoundSelector(state: RootState) {
+  const currentGame = state.currentGame
+  if (currentGame && currentGame.stage !== GameStage.ended) {
+    return currentGame.currentRound
+  }
+  return null
+}
