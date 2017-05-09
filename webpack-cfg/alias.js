44d36539a8738c81ea0a084fdf797511ac2afdd0
holi0317 <holliswuhollis@gmail.com>
2017-05-09 22:14:29 +0800

    Use lodash-es instead of normal one

--- webpack-cfg/alias.js
+++ webpack-cfg/alias.js
@@ -1,6 +1,8 @@
 module.exports = {
   resolve: {
     alias: {
+      // Use lodash-es instead of lodash
+      'lodash': 'lodash-es',
       // Use preact instead of react
       'react': 'preact-compat',
       'react-dom': 'preact-compat',
