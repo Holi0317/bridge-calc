de0b1c005ec59e41413938950af56e60eb28db04
holi0317 <holliswuhollis@gmail.com>
2017-05-20 00:22:33 +0800

    Add player id selector

---
+++ src/selectors/player-id.js
@@ -0,0 +1,18 @@
+// @flow
+import {createSelector} from 'reselect'
+
+import type {PlayerMap, RootState} from '../types'
+
+/**
+ * Select all player ID as an array.
+ * If player ID is not available, empty array will be returned
+ */
+export const playerIDSelector = createSelector(
+  (state: RootState) => state.currentGame ? state.currentGame.names : null,
+  (names: ?PlayerMap<string>) => {
+    if (!names) {
+      return []
+    }
+    return Object.keys(names)
+  }
+)
