6d95517dfe6fa55cae0598aa0bf933b45ee4d4c9
holi0317 <holliswuhollis@gmail.com>
2017-05-29 21:54:59 +0800

    Implement icon button in MDC

---
+++ src/components/icon-button.jsx
@@ -0,0 +1,28 @@
+// @flow
+import {h} from 'preact'
+import MaterialComponent from 'preact-material-components/MaterialComponent'
+
+export type IconButtonProps = {
+  icon: React$Element<any>
+}
+
+export class IconButton extends MaterialComponent {
+  props: IconButtonProps
+  control: HTMLElement
+  static defaultProps = {
+    ripple: true
+  }
+
+  componentDidMount() {
+    this.attachRipple()
+  }
+
+  render() {
+    return (
+      <div ref={control => (this.control = control)}>
+        <div className="mdc-ripple-surface" />
+        {this.props.icon}
+      </div>
+    )
+  }
+}
