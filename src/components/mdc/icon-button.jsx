13c3aad327a19d0c796162efa1a741a6c125d613
holi0317 <holliswuhollis@gmail.com>
2017-05-30 13:36:34 +0800

    Implement on-click for icon-button

--- src/components/mdc/icon-button.jsx
+++ src/components/mdc/icon-button.jsx
@@ -1,9 +1,11 @@
 // @flow
 import {h} from 'preact'
 import MaterialComponent from 'preact-material-components/MaterialComponent'
+import style from './icon-button.css'
 
 export type IconButtonProps = {
-  icon: React$Element<any>
+  icon: React$Element<any>,
+  onClick: ?() => void
 }
 
 export class IconButton extends MaterialComponent {
@@ -18,8 +20,10 @@ export class IconButton extends MaterialComponent {
   }
 
   render() {
+    const {onClick} = this.props
+    const className = `mdc-ripple-surface ${style.iconButton}`
     return (
-      <div ref={control => (this.control = control)} className="mdc-ripple-surface">
+      <div ref={control => (this.control = control)} className={className} onClick={onClick}>
         {this.props.icon}
       </div>
     )
