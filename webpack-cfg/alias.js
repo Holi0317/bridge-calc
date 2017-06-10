1cf66c89758a5482f5e339c0361a78603acdc6d1
holi0317 <holliswuhollis@gmail.com>
2017-06-10 23:46:13 +0800

    Alias react-sortable-hoc

--- webpack-cfg/alias.js
+++ webpack-cfg/alias.js
@@ -15,7 +15,8 @@ module.exports = {
       'react-css-themr': 'react-css-themr/src/index.js',
       'react-collapse': 'react-collapse/src/index.js',
       'react-height': 'react-height/src/index.js',
-      'react-i18next': 'react-i18next/src/index.js'
+      'react-i18next': 'react-i18next/src/index.js',
+      'react-sortable-hoc': 'react-sortable-hoc/dist/es6'
     }
   }
 }
