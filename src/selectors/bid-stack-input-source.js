67eef0f941bfb3375b5b3ec44fdd1d555ac8dfa4
holi0317 <holliswuhollis@gmail.com>
2017-06-12 20:52:30 +0800

    Split stack input source into multiple modules

---
+++ src/selectors/bid-stack-input-source.js
@@ -0,0 +1,48 @@
+// @flow
+import {createSelector} from 'reselect'
+import range from 'lodash/range'
+import sum from 'lodash/sum'
+import values from 'lodash/values'
+import last from 'lodash/last'
+import {stackInputSourceCommonSelector} from './stack-input-source-common'
+import {currentRoundSelector} from './current-round'
+import {bidSelector} from './bid'
+import {playerIDSelector} from './player-id'
+import {playerOrderSelector} from './player-order'
+import {createSource, fillObj} from '../utils'
+
+import type {StackInputSourceCommon} from './stack-input-source-common'
+import type {DropdownSource, PlayerMap} from '../types'
+
+/**
+ * Select input source for MDC's dropdown on bid stack.
+ */
+export const bidStackInputSourceSelector = createSelector(
+  stackInputSourceCommonSelector,
+  playerIDSelector,
+  currentRoundSelector,
+  bidSelector,
+  playerOrderSelector,
+  (common: StackInputSourceCommon, playerID: string[], currentRound: number, bidMap: PlayerMap<number>, playerOrder: string[]): PlayerMap<DropdownSource<number>[]> => {
+    if (common) {
+      return common
+    }
+    const defaultSrc = createSource(range(currentRound + 1))
+    const bid = fillObj({}, playerID, defaultSrc)
+    const bidSum: number = sum(values(bidMap))
+
+    // Surgery on bid options
+    // Rules for bridge game. Last player cannot select the one that will cause sum == currentRound
+    if (bidSum <= currentRound) {
+      const lastPlayerID: string = last(playerOrder)
+      const delta = currentRound - bidSum
+      const option = {value: delta, label: delta + '', disabled: true}
+
+      // All bid in the map points to the same object. A clone on the map is needed before mutating it.
+      const clonedOptions = bid[lastPlayerID].slice()
+      clonedOptions[delta] = option
+      bid[lastPlayerID] = clonedOptions
+    }
+    return bid
+  }
+)
---
+++ src/selectors/win-stack-input-source.js
@@ -0,0 +1,60 @@
+// @flow
+import {createSelector} from 'reselect'
+import range from 'lodash/range'
+import sum from 'lodash/sum'
+import values from 'lodash/values'
+import fromPairs from 'lodash/fromPairs'
+import {stackInputSourceCommonSelector} from './stack-input-source-common'
+import {currentRoundSelector} from './current-round'
+import {playerIDSelector} from './player-id'
+import {stageSelector} from './stage'
+import {winSelector} from './win'
+import {createSource, fillObj} from '../utils'
+import {GameStage} from '../game-stage'
+
+import type {StackInputSourceCommon} from './stack-input-source-common'
+import type {DropdownSource, PlayerMap} from '../types'
+
+/**
+ * Select input source for MDC's dropdown on win stack.
+ */
+export const winStackInputSourceSelector = createSelector(
+  stackInputSourceCommonSelector,
+  stageSelector,
+  playerIDSelector,
+  currentRoundSelector,
+  winSelector,
+  (common: StackInputSourceCommon, stage: string, playerID: string[], currentRound: number, winMap: PlayerMap<number>): PlayerMap<DropdownSource<number>[]> => {
+    if (common) {
+      return common
+    }
+    const defaultSrc = createSource(range(currentRound + 1))
+    const winSum: number = sum(values(winMap))
+
+    // Short circuit.
+    // No-op when there is excess win
+    // Or stage is not at waitingWin
+    if (stage !== GameStage.waitingWin || winSum > currentRound) {
+      return fillObj({}, playerID, defaultSrc)
+    }
+
+    // Surgery on win options
+    // Cannot choose a win that exceed currentRound
+    const delta = currentRound - winSum
+    const head = createSource(range(delta + 1))
+    const tail = range(delta + 1, currentRound + 1)
+      .map(i => ({value: i, label: i + '', disabled: true}))
+    const restSrc = head.concat(tail)
+
+    const pairs = playerID
+      .map(ID => {
+        const selectedWin = winMap[ID]
+        const src = (selectedWin <= 0 || selectedWin == null)
+          ? restSrc
+          : defaultSrc
+        return [ID, src]
+      })
+
+    return fromPairs(pairs)
+  }
+)
