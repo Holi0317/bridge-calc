2ef6aa9e66c08a0e12a99a179113db4c96f2800e
holi0317 <holliswuhollis@gmail.com>
2017-06-12 22:28:05 +0800

    Remove unused code in action button component

--- src/components/game/action-buttons.jsx
+++ src/components/game/action-buttons.jsx
@@ -4,13 +4,11 @@ import {connect} from 'preact-redux'
 import {translate} from 'react-i18next'
 import Button from 'preact-material-components/Button/Button'
 import {GameStage} from '../../game-stage'
-import {BID, SET_BID, SET_WIN, UNDO, WIN} from '../../actions/current-game'
-import {stackInputValidator} from '../../validators/stack-input'
-import {isOk, fillObj} from '../../utils'
+import {BID, UNDO, WIN} from '../../actions/current-game'
 import style from './action-buttons.css'
 
-import type {Dispatch, PlayerMap, RootState, T} from '../../types'
-import type {BID_ACTION, WIN_ACTION, UNDO_ACTION, SET_BID_ACTION, SET_WIN_ACTION} from '../../actions/current-game'
+import type {Dispatch, RootState, T} from '../../types'
+import type {BID_ACTION, WIN_ACTION, UNDO_ACTION} from '../../actions/current-game'
 import type {GameState} from '../../reducer/current-game'
 import {stageSelector} from '../../selectors/stage'
 import {validStackInput} from '../../selectors/stack-input-validator'
@@ -43,76 +41,18 @@ function mapStateToProps(state: RootState, {t}) {
   }
 }
 
-type UpdaterOpts = {
-  buffer: PlayerMap<number>,
-  bufferName: 'bid' | 'win',
-  names: PlayerMap<string>,
-  currentRound: number,
-  dispatch: Dispatch
-}
-
-/**
- * Handler function for next action
- */
-function updater(opts: UpdaterOpts) {
-  const playerIDs = Object.keys(opts.names)
-  let buffer = opts.buffer
-
-  // Check if 0 need to be filled
-  if (Object.keys(opts.buffer).length !== playerIDs.length) {
-    buffer = fillObj(opts.buffer, playerIDs, 0)
-    if (opts.bufferName === 'bid') {
-      const action: SET_BID_ACTION = {type: SET_BID, payload: buffer}
-      opts.dispatch(action)
-    } else if (opts.bufferName === 'win') {
-      const action: SET_WIN_ACTION = {type: SET_WIN, payload: buffer}
-      opts.dispatch(action)
-    }
-
-    const validateOpt = {
-      [opts.bufferName]: buffer,
-      currentRound: opts.currentRound,
-      lastPlayerID: ''  // We do not care error message. Leave this as blank string will do
-    }
-    const valid = isOk(stackInputValidator(validateOpt, a => a))
-    if (!valid) {
-      // Invalid after changing the state. Refuse to proceed
-      return
-    }
-  }
-
-  if (opts.bufferName === 'bid') {
-    const action: BID_ACTION = {type: BID, payload: buffer}
-    opts.dispatch(action)
-  } else if (opts.bufferName === 'win') {
-    const action: WIN_ACTION = {type: WIN, payload: buffer, time: new Date()}
-    opts.dispatch(action)
-  }
-}
-
 function mapDispatchToProps(dispatch: Dispatch) {
   return {
     next(state: GameState) {
       return () => {
-
         if (!state) {
           // Just to eliminate state is null case
         } else if (state.stage === GameStage.waitingBid) {
-          updater({
-            dispatch,
-            buffer: state.bid,
-            bufferName: 'bid',
-            names: state.names,
-            currentRound: state.currentRound
-          })
+          const action: BID_ACTION = {type: BID, payload: state.bid}
+          dispatch(action)
         } else if (state.stage === GameStage.waitingWin) {
-          updater({
-            dispatch,
-            buffer: state.win,
-            bufferName: 'win',
-            names: state.names,
-            currentRound: state.currentRound
-          })
+          const action: WIN_ACTION = {type: WIN, payload: state.win, time: new Date()}
+          dispatch(action)
         }
 
       }
