f6f1e922739c9e8470f15f0621fdbd020df1d69c
holi0317 <holliswuhollis@gmail.com>
2017-06-15 21:11:47 +0800

    Fix linting error

--- src/components/game/scoreboard.jsx
+++ src/components/game/scoreboard.jsx
@@ -6,7 +6,6 @@ import {namesSelector} from '../../selectors/names'
 import {playerScoresSelector} from '../../selectors/player-scores'
 import {playerTotalScoreSelector} from '../../selectors/player-total-score'
 import {playerRankSelector} from '../../selectors/player-rank'
-import {currentRoundSelector} from '../../selectors/current-round'
 import {endedRoundsArraySelector} from '../../selectors/ended-rounds-array'
 import {toPairs} from '../../utils'
 import style from './scoreboard.css'
@@ -30,41 +29,41 @@ function DisconnectScoreboard({t, names, scores, endedRounds, totalScores, ranks
         <table className={style.table}>
 
           <thead>
-          {/* Player names */}
-          <tr>
-            <th>{t('Name')}</th>
-            {toPairs(names).map(([playerID, name]) => (
-              <th key={playerID}>{name}</th>
-            ))}
-          </tr>
+            {/* Player names */}
+            <tr>
+              <th>{t('Name')}</th>
+              {toPairs(names).map(([playerID, name]) => (
+                <th key={playerID}>{name}</th>
+              ))}
+            </tr>
           </thead>
 
           <tbody>
-          {/* Scores for each round */}
-          {endedRounds.map(i => (
-            <tr key={i}>
-              <td>{i}</td>
-              {toPairs(scores).map(([playerID, scores]) => (
-                <td key={playerID}>{scores[i - 1]}</td>
+            {/* Scores for each round */}
+            {endedRounds.map(i => (
+              <tr key={i}>
+                <td>{i}</td>
+                {toPairs(scores).map(([playerID, scores]) => (
+                  <td key={playerID}>{scores[i - 1]}</td>
+                ))}
+              </tr>
+            ))}
+
+            {/* Total scores */}
+            <tr>
+              <td>{t('Total score')}</td>
+              {toPairs(totalScores).map(([playerID, total]) => (
+                <td key={playerID}>{total}</td>
               ))}
             </tr>
-          ))}
 
-          {/* Total scores */}
-          <tr>
-            <td>{t('Total score')}</td>
-            {toPairs(totalScores).map(([playerID, total]) => (
-              <td key={playerID}>{total}</td>
-            ))}
-          </tr>
-
-          {/* Rank */}
-          <tr>
-            <td>{t('Rank')}</td>
-            {toPairs(ranks).map(([playerID, rank]) => (
-              <td key={playerID}>{rank}</td>
-            ))}
-          </tr>
+            {/* Rank */}
+            <tr>
+              <td>{t('Rank')}</td>
+              {toPairs(ranks).map(([playerID, rank]) => (
+                <td key={playerID}>{rank}</td>
+              ))}
+            </tr>
           </tbody>
 
         </table>
