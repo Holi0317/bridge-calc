e922096d29eac5fbf11d5320e18aefceb7143efd
holi0317 <holliswuhollis@gmail.com>
2017-06-22 20:36:45 +0800

    Add autoprefixer to css build pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,9 @@ module.exports = {
       root: __dirname
     },
     'postcss-nesting': {},
+    'autoprefixer': {
+      browsers: ['last 2 Chrome versions', 'last 2 Firefox versions']
+    },
     'postcss-reporter': {
       clearReportedMessages: true
     }
