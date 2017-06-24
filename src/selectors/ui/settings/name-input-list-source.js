32afef7a00691e5076d73c8d22cbeac74616f7ca
holi0317 <holliswuhollis@gmail.com>
2017-06-25 00:55:53 +0800

    Add type annotation to name-input-list-source selector

--- src/selectors/ui/settings/name-input-list-source.js
+++ src/selectors/ui/settings/name-input-list-source.js
@@ -1,16 +1,28 @@
 // @flow
 import {createSelector} from 'reselect'
+import fromPairs from 'lodash/fromPairs'
 import {namesSelector} from './names'
 import toPairs from 'lodash/toPairs'
 
 import type {PlayerMap} from '../../../types'
 
 /**
+ * PlayerName structure to be passed into name-input-list component.
+ * This represent one player in the system.
+ * First string is the ID. Second string is the name.
+ */
+export type PlayerName = [string, string]
+
+export function revert(data: PlayerName[]): PlayerMap<string> {
+  return fromPairs(data)
+}
+
+/**
  * Select names source used for name-input-list component for usage in settings view.
  */
 export const nameInputListSourceSelector = createSelector(
   namesSelector,
-  (names: PlayerMap<string>) => (
+  (names: PlayerMap<string>): PlayerName[] => (
     toPairs(names)
   )
 )
