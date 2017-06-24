9f157c57a18f61cfcbf58dd3b776f4c4737247ab
holi0317 <holliswuhollis@gmail.com>
2017-06-25 00:54:46 +0800

    Add selector on valid settings state

--- src/selectors/validators/player-editor.js
+++ src/selectors/validators/player-editor.js
@@ -4,6 +4,7 @@ import {namesSelector} from '../ui/settings/names'
 import {playerEditorValidator} from '../../validators/player-editor'
 
 import type {PlayerMap, RootState, T} from '../../types'
+import type {PlayerEditorError} from '../../validators/player-editor'
 
 /**
  * Select validation result from player editor validator using state.
@@ -14,5 +15,18 @@ export const playerEditorValidatorSelector = createSelector(
   (state: RootState, t: T) => t,
   (names: PlayerMap<string>, t) => (
     playerEditorValidator({names}, t)
+  )
+)
+
+/**
+ * Test if current player editor is valid.
+ * If true, the player editor state is valid and has passed validation.
+ * False otherwise.
+ * Additional argument: i18next T object must be passed in as second argument.
+ */
+export const isValidPlayerEditor = createSelector(
+  playerEditorValidatorSelector,
+  (error: PlayerEditorError) => (
+    error.misc === '' && (Object.keys(error.names).length === 0)
   )
 )
--- test/selectors/validators/player-editor.spec.js
+++ test/selectors/validators/player-editor.spec.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import {playerEditorValidatorSelector} from '../../../src/selectors/validators/player-editor'
+import {isValidPlayerEditor, playerEditorValidatorSelector} from '../../../src/selectors/validators/player-editor'
 import {genMap} from '../../fixtures/current-game-states'
 import {t as trans} from '../../helpers/translate'
 
@@ -18,4 +18,46 @@ test('Validation result should be returned', t => {
   }
   const actual = playerEditorValidatorSelector(state, trans)
   t.deepEqual(actual, expected, 'Validation result should be selected')
+})
+
+test('isValidPlayerEditor should be true for valid data', t => {
+  const state = {
+    ui: {
+      settings: {
+        names: genMap('John', 'Mary', 'Henry', 'Joe'),
+        maker: 'a'
+      }
+    }
+  }
+  const expected = true
+  const actual = isValidPlayerEditor(state, trans)
+  t.is(actual, expected, 'True should be selected for valid data')
+})
+
+test('isValidPlayerEditor should be false for invalid data in misc', t => {
+  const state = {
+    ui: {
+      settings: {
+        names: {},
+        maker: null
+      }
+    }
+  }
+  const expected = false
+  const actual = isValidPlayerEditor(state, trans)
+  t.is(actual, expected, 'False should be selected for invalid data')
+})
+
+test('isValidPlayerEditor should be false for invalid data in names', t => {
+  const state = {
+    ui: {
+      settings: {
+        names: genMap('John', 'Mary', 'Mary', 'Joe'),
+        maker: 'a'
+      }
+    }
+  }
+  const expected = false
+  const actual = isValidPlayerEditor(state, trans)
+  t.is(actual, expected, 'False should be selected for valid data')
 })
