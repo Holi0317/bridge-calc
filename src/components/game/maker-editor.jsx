558de535ddd925b3071ffef93326c0d95bf482cd
holi0317 <holliswuhollis@gmail.com>
2017-06-25 21:54:59 +0800

    Add snackbar notification when maker has changed

--- src/components/game/maker-editor.jsx
+++ src/components/game/maker-editor.jsx
@@ -1,34 +1,66 @@
 // @flow
-import {h} from 'preact'
+import {h, Component} from 'preact'
 import {translate} from 'react-i18next'
-import style from './maker-editor.css'
-import type {Dispatch, PlayerMap, RootState, T} from '../../types'
-import {MakerChooser} from './maker-chooser'
 import {connect} from 'preact-redux'
 import Button from 'preact-material-components/Button/Button'
 import {CHANGE_PLAYERS} from '../../actions/current-game'
-import type {CHANGE_PLAYERS_ACTION} from '../../actions/current-game'
 import {namesSelector} from '../../selectors/names'
 import {roundsSelector} from '../../selectors/rounds'
 import {makerSelector} from '../../selectors/ui/settings/maker'
+import {Snackbar} from '../mdc/snackbar'
+import {MakerChooser} from './maker-chooser'
+import style from './maker-editor.css'
 
-type MakerEditorProps = {
-  names: PlayerMap<string>,
-  rounds: number,
-  maker: string,
+import type {Dispatch, PlayerMap, RootState, T} from '../../types'
+import type {CHANGE_PLAYERS_ACTION} from '../../actions/current-game'
 
-  commit: (names: PlayerMap<string>, rounds: number, maker: string) => () => void,
-  t: T
-}
+class DisconnectMakerEditor extends Component {
+  props: {
+    names: PlayerMap<string>,
+    rounds: number,
+    maker: string,
 
-function DisconnectMakerEditor({names, rounds, maker, commit, t}: MakerEditorProps) {
-  return (
-    <div>
-      <h4>{t('Change maker')}</h4>
-      <MakerChooser />
-      <Button raised accent className={style.btn} onClick={commit(names, rounds, maker)}>{t('Change maker')}</Button>
-    </div>
-  )
+    commit: (names: PlayerMap<string>, rounds: number, maker: string) => void,
+    t: T
+  }
+  state: {
+    snackbarOpen: boolean
+  }
+
+  constructor() {
+    super(...arguments)
+    this.state = {
+      snackbarOpen: false
+    }
+  }
+
+  commit = () => {
+    const {names, rounds, maker, commit} = this.props
+    commit(names, rounds, maker)
+    this.setState(() => ({
+      snackbarOpen: true
+    }))
+  }
+
+  snackbarClosed = () => {
+    this.setState(() => ({
+      snackbarOpen: false
+    }))
+  }
+
+  render() {
+    const {t} = this.props
+    const {snackbarOpen} = this.state
+
+    return (
+      <div>
+        <h4>{t('Change maker')}</h4>
+        <MakerChooser />
+        <Button raised accent className={style.btn} onClick={this.commit}>{t('Change maker')}</Button>
+        <Snackbar open={snackbarOpen} message={t('Maker changed!')} onClosed={this.snackbarClosed} />
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state: RootState) => ({
@@ -39,15 +71,13 @@ const mapStateToProps = (state: RootState) => ({
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   commit(names: PlayerMap<string>, rounds: number, maker: string) {
-    return () => {
-      const action: CHANGE_PLAYERS_ACTION = {
-        type: CHANGE_PLAYERS,
-        newNames: names,
-        maker,
-        rounds
-      }
-      dispatch(action)
+    const action: CHANGE_PLAYERS_ACTION = {
+      type: CHANGE_PLAYERS,
+      newNames: names,
+      maker,
+      rounds
     }
+    dispatch(action)
   }
 })
 
