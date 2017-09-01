94f5596bc5c4f77d74f2f7515a111794743df107
holi0317 <holliswuhollis@gmail.com>
2017-09-01 20:42:45 +0800

    Only babel transpile some libraries

--- webpack-cfg/alias.js
+++ webpack-cfg/alias.js
@@ -5,10 +5,9 @@ module.exports = {
       'reselect': 'reselect/src/index.js',
 
       // Use non-compiled version of the following react libraries
-      'react-icon-base': 'react-icon-base/index.js',
-      'react-css-themr': 'react-css-themr/src/index.js',
       'react-collapse': 'react-collapse/src/index.js',
       'react-height': 'react-height/src/index.js',
+
       'react-i18next': 'react-i18next/src/index.js',
       'react-sortable-hoc': 'react-sortable-hoc/dist/es6'
     }
--- webpack-cfg/babel.js
+++ webpack-cfg/babel.js
@@ -1,24 +1,16 @@
 const path = require('path')
 
 const libraries = [
-  'reselect',
-  'react-icons',
-  'react-icon-base',
-  'react-css-themr',
-  'react-collapse',
-  'react-height',
   'react-i18next',
-  'react-toolbox',
-  'preact-material-components'
+  'react-collapse',
+  'react-height'
 ]
 
 module.exports = {
   module: {
     rules: [{
-      test: /\.jsx?$/,
-      include: [
-        path.resolve(__dirname, '../src')
-      ].concat(libraries.map(lib => path.resolve(__dirname, '../node_modules', lib))),
+      test: /\.js$/,
+      include: libraries.map(lib => path.resolve(__dirname, '../node_modules', lib)),
       use: {
         loader: 'babel-loader',
         options: {
