85d7f3ea3b075f70e798ef9416d8d24395f75b2b
holi0317 <holliswuhollis@gmail.com>
2017-10-19 22:32:11 +0800

    Use CSS for controlling size of icon in main menu tile

--- src/main-menu/menu.tsx
+++ src/main-menu/menu.tsx
@@ -7,7 +7,7 @@ import AvSkipPrevious from 'material-ui/svg-icons/av/skip-previous'
 import ActionSettings from 'material-ui/svg-icons/action/settings'
 import ActionInfo from 'material-ui/svg-icons/action/info'
 import ActionBugReport from 'material-ui/svg-icons/action/bug-report'
-import {Tile} from '../material/tile'
+import {Tile} from './tile'
 import {ITranslateMixin} from '../types'
 import grid from '../styles/grid.css'
 
@@ -16,28 +16,28 @@ export function MenuImpl({t}: ITranslateMixin) {
     <div className={grid.container}>
       <div className={grid.row}>
         <div className={grid.col12}>
-          <Tile icon={AvPlayArrow} title={t('Continue')} />
+          <Tile icon={<AvPlayArrow />} title={t('Continue')} />
         </div>
       </div>
 
       <div className={grid.row}>
         <div className={`${grid.colLg6} ${grid.colMd12}`}>
-          <Tile icon={AvFiberNew} title={t('New Game')} to="/entry" />
+          <Tile icon={<AvFiberNew />} title={t('New Game')} to="/entry" />
         </div>
         <div className={`${grid.colLg6} ${grid.colMd12}`}>
-          <Tile icon={AvSkipPrevious} title={t('Previous games')} to="/prev-games" />
+          <Tile icon={<AvSkipPrevious />} title={t('Previous games')} to="/prev-games" />
         </div>
       </div>
 
       <div className={grid.row}>
         <div className={`${grid.colLg4} ${grid.colMd12}`}>
-          <Tile icon={ActionSettings} title={t('Settings')} />
+          <Tile icon={<ActionSettings />} title={t('Settings')} />
         </div>
         <div className={`${grid.colLg4} ${grid.colMd12}`}>
-          <Tile icon={ActionInfo} title={t('Information')} />
+          <Tile icon={<ActionInfo />} title={t('Information')} />
         </div>
         <div className={`${grid.colLg4} ${grid.colMd12}`}>
-          <Tile icon={ActionBugReport} title={t('Support/bug')} />
+          <Tile icon={<ActionBugReport />} title={t('Support/bug')} to="https://github.com/holi0317/bridge-calc" />
         </div>
       </div>
 
--- src/main-menu/tile.tsx
+++ src/main-menu/tile.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react'
 import {Link} from 'react-router-dom'
-import SvgIcon from 'material-ui/SvgIcon'
 import TouchRipple from 'material-ui/internal/TouchRipple'
 import style from './tile.css'
 
-const iconStyle = {
-  width: 92,
-  height: 92
-}
-
 export interface ITileProps {
   title: string,
-  icon: typeof SvgIcon,
+  icon: React.ReactElement<{}>,
   to?: string
 }
 
@@ -22,14 +16,13 @@ export interface ITileProps {
  * @param props.to - Path of the link that this tile links to
  */
 export function Tile(props: ITileProps) {
-  const title = props.title
-  const MyIcon = props.icon
+  const {title, icon} = props
   const to = props.to || '/'
   return (
     <Link to={to} className={style.link}>
       <TouchRipple>
         <div className={style.tile}>
-          <div className={style.iconContainer}><MyIcon style={iconStyle} /></div>
+          <div className={style.iconContainer}>{icon}</div>
           <div className={style.titleContainer}>{title}</div>
         </div>
       </TouchRipple>
