f84eaa861e55b5e258016c9c88e2b7bd2c03ec25
holi0317 <holliswuhollis@gmail.com>
2017-10-24 21:33:37 +0800

    Fix improper use of react-loadable component

--- src/lazy-views/loading.tsx
+++ src/lazy-views/loading.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react'
+import {LoadingComponentProps} from 'react-loadable'
 import {Spinner} from '../material/spinner'
 import style from './loading.css'
+import {Ouch} from '../error-boundary/ouch'
 
-export function Loading() {
-  return (
-    <div className={style.container}>
-      <Spinner />
-    </div>
-  )
+export function Loading({error, pastDelay}: LoadingComponentProps) {
+  if (error) {
+    return <Ouch error="Failed to load route." />
+  }
+  if (pastDelay) {
+    return (
+      <div className={style.container}>
+        <Spinner />
+      </div>
+    )
+  }
+  return null
 }
