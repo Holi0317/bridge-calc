9a5854ecb1360f3aba5584d52803127d82a1982c
holi0317 <holliswuhollis@gmail.com>
2017-10-27 21:39:09 +0800

    Add alias for lodash libraries

--- webpack-cfg/alias.js
+++ webpack-cfg/alias.js
@@ -1,7 +1,12 @@
 module.exports = {
   resolve: {
     alias: {
+      // Alias lodash
+      // Use ES-version of lodash for tree shaking (Probably wont do much. But at least unifying the version of lodash)
       'lodash': 'lodash-es',
+      // The following two libraries are used by material-ui
+      'lodash.merge': 'lodash-es/merge',
+      'lodash.throttle': 'lodash-es/throttle',
 
       // Use non-compiled version of the following libraries
       'reselect': 'reselect/src/index.js',
