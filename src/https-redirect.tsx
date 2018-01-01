dc72eaeee87c378b2efe3df7d9bdf6b76ab3462b
holi0317 <holliswuhollis@gmail.com>
2018-01-01 23:29:15 +0800

    Add https redirect

---
+++ src/https-redirect.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+function isLocalHost(hostname: string) {
+  return !!(hostname === 'localhost' ||
+    hostname === '[::1]' ||
+    hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/))
+}
+
+export class HttpsRedirect extends React.Component {
+  public constructor(props: any) {
+    super(props)
+    if (
+      typeof window !== 'undefined' &&
+      window.location &&
+      window.location.protocol === 'http:' &&
+      !isLocalHost(window.location.hostname)
+    ) {
+      window.location.href = window.location.href.replace(/^http(?!s)/, 'https')
+    }
+  }
+
+  public render() {
+    return this.props.children
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom'
 import {I18nextProvider} from 'react-i18next'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {PersistGate} from 'redux-persist/es/integration/react'
+import {HttpsRedirect} from './https-redirect'
 import {Shell} from './shell'
 import {i18n} from './app/i18n'
 import {store, persistor} from './app/redux-store'
@@ -13,19 +14,21 @@ import {ErrorBoundary} from './error-boundary'
 
 function Root() {
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <I18nextProvider i18n={i18n}>
-          <BrowserRouter>
-            <MuiThemeProvider muiTheme={muiTheme}>
-              <ErrorBoundary>
-                <Shell />
-              </ErrorBoundary>
-            </MuiThemeProvider>
-          </BrowserRouter>
-        </I18nextProvider>
-      </PersistGate>
-    </Provider>
+    <HttpsRedirect>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <I18nextProvider i18n={i18n}>
+            <BrowserRouter>
+              <MuiThemeProvider muiTheme={muiTheme}>
+                <ErrorBoundary>
+                  <Shell />
+                </ErrorBoundary>
+              </MuiThemeProvider>
+            </BrowserRouter>
+          </I18nextProvider>
+        </PersistGate>
+      </Provider>
+    </HttpsRedirect>
   )
 }
 
