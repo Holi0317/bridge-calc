9303569caf9e72d9b3a0c17de34c7080f992e71b
holi0317 <holliswuhollis@gmail.com>
2018-01-01 22:44:10 +0800

    Disable route transition to fix issue with score-input route

--- src/shell/routes.tsx
+++ src/shell/routes.tsx
@@ -1,37 +1,22 @@
 import * as React from 'react'
-import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import {Location} from 'history'
 import {ErrorBoundary} from '../error-boundary'
 import {Route, Switch} from 'react-router'
 import {routes} from '../app/routes'
-import styles from './route-transitions.css'
-
-const transitionClass = {
-  appear: styles.appear,
-  appearActive: styles.appearActive,
-  enter: styles.enter,
-  enterActive: styles.enterActive
-}
 
 interface IRoutesProps {
   location: Location
 }
 
 export function Routes({location}: IRoutesProps) {
-  // `/score-input` route has its own transition/animation logic.
-  // Blacklisting it from general transition
-  const transitionDisabled = location.pathname.startsWith('/score-input')
+  // FIXME Add transition
+  // Got some issue with transition in production build
+  // Ref: 314974f089de50901b6d6422c5b9edc682236104
   return <ErrorBoundary>
-    <TransitionGroup exit={false} enter={!transitionDisabled}>
-      <CSSTransition classNames={transitionClass} key={location.key} timeout={300} appear>
-        <div className={styles.base}>
-          <Switch location={location}>
-            {routes.map(({title, name, ...rest}) => (
-              <Route key={name} {...rest} />
-            ))}
-          </Switch>
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
+    <Switch location={location}>
+      {routes.map(({title, name, ...rest}) => (
+        <Route key={name} {...rest} />
+      ))}
+    </Switch>
   </ErrorBoundary>
 }
