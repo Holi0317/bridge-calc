dfa25e23b43c3464b8cd18fc70f5bd69e8e9644d
holi0317 <holliswuhollis@gmail.com>
2018-01-06 21:27:47 +0800

    Add test fixtures for data in v1.0.0

---
+++ test-fixtures/migration-data.js
@@ -0,0 +1,15 @@
+export const round1 = {
+  'Bridge.currentRound': '1',
+  'Bridge.maker': '0',
+  'Bridge.players': '[{"bid":null,"win":null,"_buffer":{"lastRound":null,"combo":null,"miss":null,"maxCombo":null,"sum":null},"score":[],"name":"Jonathan"},{"bid":null,"win":null,"_buffer":{"lastRound":null,"combo":null,"miss":null,"maxCombo":null,"sum":null},"score":[],"name":"Helen"},{"bid":null,"win":null,"_buffer":{"lastRound":null,"combo":null,"miss":null,"maxCombo":null,"sum":null},"score":[],"name":"Humphrey"},{"bid":null,"win":null,"_buffer":{"lastRound":null,"combo":null,"miss":null,"maxCombo":null,"sum":null},"score":[],"name":"Margery"}]',
+  'Bridge.state': '1',
+  'Bridge.totalRounds': '13'
+}
+
+export const round2 = {
+  'Bridge.currentRound':'2',
+  'Bridge.maker':'1',
+  'Bridge.players':'[{"bid":null,"win":null,"_buffer":{"lastRound":1,"combo":1,"miss":0,"maxCombo":0,"sum":11},"score":[11],"name":"Jonathan"},{"bid":null,"win":null,"_buffer":{"lastRound":1,"combo":0,"miss":1,"maxCombo":0,"sum":-1},"score":[-1],"name":"Helen"},{"bid":null,"win":null,"_buffer":{"lastRound":1,"combo":0,"miss":1,"maxCombo":0,"sum":-1},"score":[-1],"name":"Humphrey"},{"bid":null,"win":null,"_buffer":{"lastRound":1,"combo":0,"miss":1,"maxCombo":0,"sum":-1},"score":[-1],"name":"Margery"}]',
+  'Bridge.state':'1',
+  'Bridge.totalRounds':'13'
+}
