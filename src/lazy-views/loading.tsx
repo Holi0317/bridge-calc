f8826ecbfd4a8a7663d81f8b53493faa1f044eac
holi0317 <holliswuhollis@gmail.com>
2018-02-09 23:22:14 +0800

    Upgrade deps

--- src/lazy-views/loading.tsx
+++ src/lazy-views/loading.tsx
@@ -3,8 +3,8 @@ import {Spinner} from '../material/spinner'
 import style from './loading.css'
 import {Ouch} from '../error-boundary/ouch'
 
-// Loadable is the namespace for react-loadable
-export function Loading({error, pastDelay}: Loadable.LoadingComponentProps) {
+// LoadableExport is the namespace for react-loadable
+export function Loading({error, pastDelay}: LoadableExport.LoadingComponentProps) {
   if (error) {
     return <Ouch error={new Error('Failed to load route.')} />
   }
