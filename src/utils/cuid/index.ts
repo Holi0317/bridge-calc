c8cb9b6ff549f69eb37bdf01f10f82ab678feff0
holi0317 <holliswuhollis@gmail.com>
2018-06-08 23:11:10 +0800

    Fix linting issue

--- src/utils/cuid/index.ts
+++ src/utils/cuid/index.ts
@@ -22,53 +22,50 @@ const blockSize = 4
 const base = 36
 const discreteValues = Math.pow(base, blockSize)
 
-function randomBlock () {
+function randomBlock() {
+  // tslint:disable-next-line:no-bitwise
   return pad((Math.random() * discreteValues << 0)
     .toString(base), blockSize)
 }
 
-function safeCounter () {
-  c = c < discreteValues ? c : 0;
-  c++; // this is not subliminal
-  return c - 1;
+function safeCounter() {
+  c = c < discreteValues ? c : 0
+  c++
+  return c - 1
 }
 
-export function cuid () {
+export function cuid(): string {
   // Make cuid behave a simple counter in testing environment for easier snapshot testing.
   if (process.env.NODE_ENV === 'test') {
     return '' + c++
   }
 
-  // Starting with a lowercase letter makes
-  // it HTML element ID friendly.
-  const letter = 'c' // hard-coded allows for sequential access
+  // Starting with a lowercase letter makes it HTML element ID friendly.
+  const letter = 'c'
 
-  // timestamp
-  // warning: this exposes the exact date and time
-  // that the uid was created.
+  // Warning: this exposes the exact date and time that the uid was created.
   const timestamp = (new Date().getTime()).toString(base)
 
   // Prevent same-machine collisions.
   const counter = pad(safeCounter().toString(base), blockSize)
 
-  // A few chars to generate distinct ids for different
-  // clients (so different computers are far less
-  // likely to generate the same id)
+  // A few chars to generate distinct ids for different clients
+  // (so different computers are far less likely to generate the same id)
   const print = fingerprint()
 
   // Grab some more chars from Math.random()
   const random = randomBlock() + randomBlock()
 
-  return letter + timestamp + counter + print + random;
+  return letter + timestamp + counter + print + random
 }
 
-export function slug () {
-  var date = new Date().getTime().toString(36)
+export function slug(): string {
+  const date = new Date().getTime().toString(36)
   const counter = safeCounter().toString(36).slice(-4)
   const print = fingerprint().slice(0, 1) + fingerprint().slice(-1)
-  const random = randomBlock().slice(-2);
+  const random = randomBlock().slice(-2)
 
-  return date.slice(-2) + counter + print + random;
+  return date.slice(-2) + counter + print + random
 }
 
 export {fingerprint}
