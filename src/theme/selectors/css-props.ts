df2a2e4b81a68a2817e0d5b8beed0e7abbe4b671
holi0317 <holliswuhollis@gmail.com>
2018-06-10 00:23:21 +0800

    Fix hover color regression

--- src/theme/selectors/css-props.ts
+++ src/theme/selectors/css-props.ts
@@ -3,18 +3,11 @@ import {activatedThemeSelector} from './activated-theme'
 import {Theme} from '@material-ui/core/styles'
 import red from '@material-ui/core/colors/red'
 import {light as lightPalette} from '@material-ui/core/styles/createPalette'
+import {fade} from '@material-ui/core/styles/colorManipulator'
 
 const white = '#fff'
 const darkBlack = 'rgba(0, 0, 0, 0.87)'
 const fullWhite = 'rgba(255, 255, 255, 1)'
-
-/**
- * Add transparency to given color.
- */
-function fade(color: string, _opacity: number): string {
-  // FIXME Add the logic
-  return color
-}
 
 /**
  * Select a mapping of css properties to values according to selected theme.
@@ -26,7 +19,6 @@ export const cssPropsSelector = createSelector(
     return new Map([
       ['--bg-color', palette.background.default || white],
       ['--text-color', palette.text.primary || fullWhite],
-      // FIXME Regression: --hover-color is not correctly computed
       ['--hover-color', fade(palette.text.primary || darkBlack, 0.1)],
       ['--error-text-color', red[500]]
     ])
