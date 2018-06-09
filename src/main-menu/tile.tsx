cdee4daf58c8b2157df7afbec77baa0d873035f2
holi0317 <holliswuhollis@gmail.com>
2018-06-10 01:58:30 +0800

    Fix tile icon color not respecting theme

--- src/main-menu/tile.tsx
+++ src/main-menu/tile.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react'
 import {TileLink} from './tile-link'
 import style from './tile.css'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography/Typography'
+import {withStyles, Theme} from '@material-ui/core/styles'
+
+const jssStyles = (theme: Theme) => ({
+  icon: {
+    color: theme.palette.getContrastText(theme.palette.background.paper)
+  }
+})
 
 export interface ITileProps {
   /** Title of the tile */
   title: string
   /** Icon for the title, in React element format */
   icon: React.ReactNode
+  /** CSS Classes injected by jss */
+  classes: any
   /** Path of the link for this tile links to */
   to?: string
   /** `true` for external (not in the scope of this app) link */
@@ -18,17 +29,20 @@ export interface ITileProps {
 /**
  * A tile represents a block on menu
  */
-export function Tile(props: ITileProps) {
-  const {title, icon, external, target} = props
-  const to = props.to || '/'
+export class TileImpl extends React.Component<ITileProps> {
+  public render() {
+    const {title, icon, external, target, classes} = this.props
+    const to = this.props.to || '/'
 
-  // FIXME Regression: icon color does not respect dark theme
-  return (
-    <TileLink to={to} external={external} target={target}>
-      <div className={style.tile}>
-        <div className={style.iconContainer}>{icon}</div>
-        <div className={style.titleContainer}>{title}</div>
-      </div>
-    </TileLink>
-  )
+    return (
+      <TileLink to={to} external={external} target={target}>
+        <Paper className={style.tile} elevation={4}>
+          <div className={`${style.iconContainer} ${classes.icon}`}>{icon}</div>
+          <Typography className={style.titleContainer} variant="title">{title}</Typography>
+        </Paper>
+      </TileLink>
+    )
+  }
 }
+
+export const Tile = withStyles(jssStyles)(TileImpl)
