06dd7aac563bee70ae6bb514dea712fd306d37cc
holi0317 <holliswuhollis@gmail.com>
2018-06-10 02:34:46 +0800

    Add more css properties and use them

--- src/main-menu/tile.tsx
+++ src/main-menu/tile.tsx
@@ -3,21 +3,12 @@ import {TileLink} from './tile-link'
 import style from './tile.css'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography/Typography'
-import {withStyles, Theme} from '@material-ui/core/styles'
-
-const jssStyles = (theme: Theme) => ({
-  icon: {
-    color: theme.palette.getContrastText(theme.palette.background.paper)
-  }
-})
 
 export interface ITileProps {
   /** Title of the tile */
   title: string
   /** Icon for the title, in React element format */
   icon: React.ReactNode
-  /** CSS Classes injected by jss */
-  classes: any
   /** Path of the link for this tile links to */
   to?: string
   /** `true` for external (not in the scope of this app) link */
@@ -29,20 +20,18 @@ export interface ITileProps {
 /**
  * A tile represents a block on menu
  */
-export class TileImpl extends React.Component<ITileProps> {
+export class Tile extends React.Component<ITileProps> {
   public render() {
-    const {title, icon, external, target, classes} = this.props
+    const {title, icon, external, target} = this.props
     const to = this.props.to || '/'
 
     return (
       <TileLink to={to} external={external} target={target}>
         <Paper className={style.tile} elevation={4}>
-          <div className={`${style.iconContainer} ${classes.icon}`}>{icon}</div>
+          <div className={style.iconContainer}>{icon}</div>
           <Typography className={style.titleContainer} variant="title">{title}</Typography>
         </Paper>
       </TileLink>
     )
   }
 }
-
-export const Tile = withStyles(jssStyles)(TileImpl)
--- src/theme/selectors/css-props.ts
+++ src/theme/selectors/css-props.ts
@@ -1,26 +1,19 @@
 import {createSelector} from 'reselect'
 import {activatedThemeSelector} from './activated-theme'
 import {Theme} from '@material-ui/core/styles'
-import red from '@material-ui/core/colors/red'
-import {light as lightPalette} from '@material-ui/core/styles/createPalette'
-import {fade} from '@material-ui/core/styles/colorManipulator'
-
-const white = '#fff'
-const darkBlack = 'rgba(0, 0, 0, 0.87)'
-const fullWhite = 'rgba(255, 255, 255, 1)'
 
 /**
  * Select a mapping of css properties to values according to selected theme.
  */
 export const cssPropsSelector = createSelector(
   activatedThemeSelector,
-  (theme: Theme): Map<string, string> => {
-    const palette = (theme.palette || lightPalette)!
+  ({palette}: Theme): Map<string, string> => {
     return new Map([
-      ['--bg-color', palette.background.default || white],
-      ['--text-color', palette.text.primary || fullWhite],
-      ['--hover-color', fade(palette.text.primary || darkBlack, 0.1)],
-      ['--error-text-color', red[500]]
+      ['--bg-color', palette.background.default],
+      ['--text-color', palette.text.primary],
+      ['--selected-color', palette.action.selected],
+      ['--error-text-color', palette.error.main],
+      ['--contrast-text-color', palette.getContrastText(palette.background.default)]
     ])
   }
 )
