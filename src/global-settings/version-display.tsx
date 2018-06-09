e40006549b5db3a1071598ed9063d3b4aa85c168
holi0317 <holliswuhollis@gmail.com>
2018-06-10 01:06:54 +0800

    Make version display secondary text

--- src/global-settings/version-display.tsx
+++ src/global-settings/version-display.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {translate} from 'react-i18next'
 import {ITranslateMixin} from '../types'
+import Typography from '@material-ui/core/Typography'
 import style from './version-display.css'
 
 // Variables is injected by webpack
@@ -8,10 +9,11 @@ declare const VERSION: string
 declare const HASH: string
 
 export function VersionDisplayImpl({t}: ITranslateMixin) {
-  // TODO Make the text secondary
   return (
     <div className={style.text}>
-      {t('Version: {{version}}', {version: `${VERSION}-${HASH}`})}
+      <Typography variant="caption">
+        {t('Version: {{version}}', {version: `${VERSION}-${HASH}`})}
+      </Typography>
     </div>
   )
 }
