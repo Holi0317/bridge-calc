1fa5302e723dd380c860e8d27ee2da3ba3ee932b
holi0317 <holliswuhollis@gmail.com>
2018-06-10 19:57:27 +0800

    Use button in name import list

--- src/entry/import-names/name-list-entry.tsx
+++ src/entry/import-names/name-list-entry.tsx
@@ -4,6 +4,7 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {translate} from 'react-i18next'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 import {setImportOpenAction, setPlayerNamesAction} from '../actions/set-entry-props'
 import {showToastAction} from '../../toast-singleton/actions/show-toast'
 import {Dispatch, ITranslateMixin} from '../../types'
@@ -25,7 +26,9 @@ export class NameListEntryImpl extends React.Component {
   public props: INameEntryProps & dispatchType & ITranslateMixin
 
   public render() {
-    return <ListItem onClick={this.setNames}>{this.props.name.join(', ')}</ListItem>
+    return <ListItem button onClick={this.setNames}>
+      <ListItemText primary={this.props.name.join(', ')} />
+    </ListItem>
   }
 
   private setNames = () => {
