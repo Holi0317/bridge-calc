57c5abf86a3c8efbb533a7cd4b66c8da87a0cdfe
holi0317 <holliswuhollis@gmail.com>
2018-06-10 15:16:23 +0800

    Fix appbar layout issue

--- src/shell/app-shell.tsx
+++ src/shell/app-shell.tsx
@@ -1,66 +1,19 @@
 import * as React from 'react'
-import flowRight from 'lodash-es/flowRight'
 import {withRouter} from 'react-router-dom'
-import {translate} from 'react-i18next'
 import {RouteComponentProps} from 'react-router'
-import {History} from 'history'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import Tooltip from '@material-ui/core/Tooltip'
-import ActionHelp from '@material-ui/icons/Help'
-import NavigationArrowBack from '@material-ui/icons/ArrowBack'
 import {ToastSingleton} from '../toast-singleton'
-import {Titles} from './titles'
 import {SWReg} from '../sw-reg'
 import {Migration} from '../migration/migration'
 import {Routes} from './routes'
-import {ITranslateMixin} from '../types'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import {Navbar} from './navbar'
 
-/**
- * Create a function that will route to specified location.
- * Think of this as the Link component in react-router. But in function form.
- * @param history - History object from react-router
- * @param loc - Path of the desired location
- * @returns - Function to redirect to the location above
- */
-function to(history: History, loc: string) {
-  return () => history.push(loc)
-}
+type AppProps = RouteComponentProps<any>
 
-type AppProps = RouteComponentProps<any> & ITranslateMixin
-
-export function ShellImpl({location, history, t}: AppProps) {
+export function ShellImpl({location}: AppProps) {
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          {/* FIXME Regression: Need proper aligning of icons */}
-          {location.pathname !== '/' && (
-            <Tooltip title={t('Back to menu')}>
-              <IconButton onClick={to(history, '/')}>
-                <NavigationArrowBack width="24px" height="24px" />
-              </IconButton>
-            </Tooltip>
-          )}
-
-          {/* FIXME Regression: Title color does not respect dark theme */}
-          {/* FIXME Regression: Title font size is a bit smaller */}
-          <Typography variant="title" color="inherit">
-            <Titles />
-          </Typography>
-
-          <Tooltip title={t('Help')}>
-            <IconButton rel="noopener" target="_blank"
-                        href="https://gitlab.com/holi0317/bridge-calc/blob/master/docs/en.md">
-              <ActionHelp width="24px" height="24px" />
-            </IconButton>
-          </Tooltip>
-        </Toolbar>
-      </AppBar>
-
+      <Navbar />
       <main>
         <Routes location={location} />
       </main>
@@ -73,7 +26,4 @@ export function ShellImpl({location, history, t}: AppProps) {
   )
 }
 
-export const Shell = flowRight(
-  withRouter,
-  translate()
-)(ShellImpl)
+export const Shell = withRouter(ShellImpl)
---
+++ src/shell/navbar.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import flowRight from 'lodash-es/flowRight'
+import {RouteComponentProps} from 'react-router'
+import {withRouter} from 'react-router-dom'
+import {translate} from 'react-i18next'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import Tooltip from '@material-ui/core/Tooltip'
+import ActionHelp from '@material-ui/icons/Help'
+import NavigationArrowBack from '@material-ui/icons/ArrowBack'
+import {Titles} from './titles'
+import {ITranslateMixin} from '../types'
+import styles from './navbar.css'
+
+type NavbarProps = RouteComponentProps<any> & ITranslateMixin
+
+export function NavbarImpl({t, history}: NavbarProps) {
+  const hasBackBtn = location.pathname !== '/'
+
+  return <AppBar position="static">
+    <Toolbar className={styles.toolbar}>
+      {hasBackBtn && (
+        <Tooltip title={t('Back to menu')}>
+          <IconButton className={styles.backBtn} onClick={() => history.push('/')}>
+            <NavigationArrowBack width="24px" height="24px" />
+          </IconButton>
+        </Tooltip>
+      )}
+
+      <Typography variant="title" color="inherit" className={styles.title}>
+        <Titles />
+      </Typography>
+
+      <Tooltip title={t('Help')}>
+        <IconButton rel="noopener" target="_blank"
+                    href="https://gitlab.com/holi0317/bridge-calc/blob/master/docs/en.md">
+          <ActionHelp width="24px" height="24px" />
+        </IconButton>
+      </Tooltip>
+    </Toolbar>
+  </AppBar>
+}
+
+export const Navbar = flowRight(
+  withRouter,
+  translate()
+)(NavbarImpl)
