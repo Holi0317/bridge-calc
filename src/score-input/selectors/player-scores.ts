0a8c4cfcd83a673325be0d2cb321d196e627d233
holi0317 <holliswuhollis@gmail.com>
2018-06-13 22:10:52 +0800

    Remove unused selectors

--- src/score-input/selectors/player-scores.ts
+++
@@ -1,8 +0,0 @@
-import {IPlayerMap, IRootState} from '../../types'
-
-/**
- * Select all player's scores.
- * Empty object will be produced for null state
- */
-export const playerScoresSelector = (state: IRootState): IPlayerMap<number[]> =>
-  state.currentGame ? state.currentGame.scores : {}
