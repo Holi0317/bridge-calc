b3b80ccbc23ba4f54e6875f5a9e1202b87d76405
holi0317 <holliswuhollis@gmail.com>
2018-06-14 22:53:16 +0800

    Apply some styling improvement to scoreboard

--- src/score-input/scoreboard/scoreboard-table.tsx
+++ src/score-input/scoreboard/scoreboard-table.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+import flowRight from 'lodash-es/flowRight'
 import {translate} from 'react-i18next'
+import {Theme, withStyles} from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableHead from '@material-ui/core/TableHead'
@@ -13,10 +15,17 @@ import classes from './scoreboard.pcss'
 
 interface IScoreboardTableProps extends ITranslateMixin {
   entry: NonNullable<GameState>,
-  mini: boolean
+  mini: boolean,
+  classes: {[className: string]: string}
 }
 
-export function ScoreboardTableImpl({entry, mini, t}: IScoreboardTableProps) {
+const styles = (theme: Theme) => ({
+  errorText: {
+    color: theme.palette.error.main
+  }
+})
+
+export function ScoreboardTableImpl({entry, mini, classes: cls, t}: IScoreboardTableProps) {
   const {names, scores, prevScores, totalScores, ranks, endedRounds} = computeData(entry)
 
   return (
@@ -24,50 +33,59 @@ export function ScoreboardTableImpl({entry, mini, t}: IScoreboardTableProps) {
       <Table className={classes.table}>
 
         <TableHead>
-        {/* Player names */}
-        <TableRow>
-          <TableCell>{t('Name')}</TableCell>
-          {Object.entries(names).map(([playerID, name]) => (
-            <TableCell key={playerID}>{name}</TableCell>
-          ))}
-        </TableRow>
+          {/* Player names */}
+          <TableRow>
+            <TableCell>{t('Name')}</TableCell>
+            {Object.entries(names).map(([playerID, name]) => (
+              <TableCell key={playerID}>{name}</TableCell>
+            ))}
+          </TableRow>
         </TableHead>
 
         <TableBody>
 
-        {mini ? (
-          <TableRow>
-            <TableCell>{t('Previous round score')}</TableCell>
-            {Object.entries(prevScores).map(([playerID, score]) => (
-              <TableCell numeric key={playerID}>{score}</TableCell>
+          {mini ? (
+            <TableRow>
+              <TableCell>{t('Previous round score')}</TableCell>
+              {Object.entries(prevScores).map(([playerID, score]) => (
+                <TableCell numeric key={playerID}
+                           className={score < 0 ? cls.errorText : ''}>
+                  {score}
+                </TableCell>
+              ))}
+            </TableRow>
+          ) : (
+            endedRounds.map(i => (
+              <tr key={i}>
+                <TableCell>{t('Round {{n}}', {n: i})}</TableCell>
+                {Object.entries(scores).map(([playerID, score]) => (
+                  <TableCell numeric key={playerID}
+                             className={score[i - 1] < 0 ? cls.errorText : ''}>
+                    {score[i - 1]}
+                  </TableCell>
+                ))}
+              </tr>
+            ))
+          )}
+
+          {/* Total scores */}
+          <TableRow className={classes.totalScore}>
+            <TableCell>{t('Total score')}</TableCell>
+            {Object.entries(totalScores).map(([playerID, total]) => (
+              <TableCell numeric key={playerID}
+                         className={total < 0 ? cls.errorText : ''}>
+                {total}
+              </TableCell>
             ))}
           </TableRow>
-        ) : (
-          endedRounds.map(i => (
-            <tr key={i}>
-              <TableCell>{t('Round {{n}}', {n: i})}</TableCell>
-              {Object.entries(scores).map(([playerID, score]) => (
-                <TableCell numeric key={playerID}>{score[i - 1]}</TableCell>
-              ))}
-            </tr>
-          ))
-        )}
 
-        {/* Total scores */}
-        <TableRow>
-          <TableCell>{t('Total score')}</TableCell>
-          {Object.entries(totalScores).map(([playerID, total]) => (
-            <TableCell numeric key={playerID}>{total}</TableCell>
-          ))}
-        </TableRow>
-
-        {/* Rank */}
-        <TableRow>
-          <TableCell>{t('Rank')}</TableCell>
-          {Object.entries(ranks).map(([playerID, rank]) => (
-            <TableCell numeric key={playerID}>{rank}</TableCell>
-          ))}
-        </TableRow>
+          {/* Rank */}
+          <TableRow>
+            <TableCell>{t('Rank')}</TableCell>
+            {Object.entries(ranks).map(([playerID, rank]) => (
+              <TableCell numeric key={playerID}>{rank}</TableCell>
+            ))}
+          </TableRow>
         </TableBody>
 
       </Table>
@@ -75,4 +93,7 @@ export function ScoreboardTableImpl({entry, mini, t}: IScoreboardTableProps) {
   )
 }
 
-export const ScoreboardTable = translate()(ScoreboardTableImpl)
+export const ScoreboardTable = flowRight(
+  withStyles(styles),
+  translate()
+)(ScoreboardTableImpl)
