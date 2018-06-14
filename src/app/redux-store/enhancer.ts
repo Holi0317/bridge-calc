388c869ce2136d3ef13f558d21562d4824d7e557
holi0317 <holliswuhollis@gmail.com>
2018-06-14 23:21:24 +0800

    Remove redux-logger

--- src/app/redux-store/enhancer.ts
+++ src/app/redux-store/enhancer.ts
@@ -11,13 +11,8 @@ const middlewares: any[] = [
 
 export function devEnhancer() {
   const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-  // tslint:disable-next-line:no-var-requires no-implicit-dependencies no-require-imports
-  const {createLogger} = require('redux-logger')
-  const logger = createLogger({
-    collapsed: true
-  })
   return composeEnhancers(
-    applyMiddleware(...[...middlewares, logger])
+    applyMiddleware(...middlewares)
   )
 }
 
