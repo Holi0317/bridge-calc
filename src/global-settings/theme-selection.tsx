9a461644a868ba4c62f2d36f8cd16dfbde187f98
holi0317 <holliswuhollis@gmail.com>
2018-06-17 11:24:12 +0800

    Add toggle button for dark theme

--- src/global-settings/theme-selection.tsx
+++ src/global-settings/theme-selection.tsx
@@ -3,28 +3,45 @@ import flowRight from 'lodash-es/flowRight'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {translate} from 'react-i18next'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
+import FormGroup from '@material-ui/core/FormGroup'
 import {Dropdown} from '../material/dropdown'
 import {themeSourcesSelector} from '../theme/selectors/theme-sources'
 import {selectedThemeSelector} from '../theme/selectors/selected-theme'
+import {isDarkThemeSelector} from '../theme/selectors/is-dark-theme'
 import {setThemeAction} from '../theme/actions/set-theme'
+import {toggleDarkAction} from '../theme/actions/toggle-dark'
 import {IRootState, ITranslateMixin, Dispatch} from '../types'
 
 const mapStateToProps = (state: IRootState) => ({
-  selectedTheme: selectedThemeSelector(state)
+  selectedTheme: selectedThemeSelector(state),
+  dark: isDarkThemeSelector(state)
 })
 
 const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators({setTheme: setThemeAction}, dispatch)
+  bindActionCreators({
+    setTheme: setThemeAction,
+    toggleDark: toggleDarkAction
+  }, dispatch)
 
 type stateType = ReturnType<typeof mapStateToProps>
 type dispatchType = ReturnType<typeof mapDispatchToProps>
 
 type ThemeSelectionProps = stateType & dispatchType & ITranslateMixin
 
-export function ThemeSelectionImpl({selectedTheme, setTheme, t}: ThemeSelectionProps) {
-  return <Dropdown label={t('Change theme')}
-                   source={themeSourcesSelector(t)} value={selectedTheme}
-                   onChange={setTheme} />
+export function ThemeSelectionImpl({selectedTheme, dark, setTheme, toggleDark, t}: ThemeSelectionProps) {
+  return (
+    <>
+      <Dropdown label={t('Change theme')}
+                source={themeSourcesSelector(t)} value={selectedTheme}
+                onChange={setTheme} />
+      <FormGroup>
+        <FormControlLabel label={t('Use dark theme')}
+                          control={<Switch checked={dark} onChange={toggleDark} color="primary" />} />
+      </FormGroup>
+    </>
+  )
 }
 
 export const ThemeSelection = flowRight(
