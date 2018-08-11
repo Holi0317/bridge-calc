89fd23d95d425e143b6b4df20507d929ecac861d
holi0317 <holliswuhollis@gmail.com>
2018-08-12 01:22:14 +0800

    Fix unit test

--- src/app/locals.spec.js
+++ src/app/locals.spec.js
@@ -2,18 +2,6 @@ import * as path from "path";
 import * as fs from "fs";
 import { languages } from "./languages";
 
-function readFile(file) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, "utf8", (err, content) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(content);
-      }
-    });
-  });
-}
-
 test("All languages should have their local file", done => {
   const files = languages.map(lang => `${lang}.js`);
   const dir = path.join(__dirname, "locals");
@@ -52,20 +40,4 @@ test("All locals should have same entries", () => {
       // We are not sorting here because key order should be preserved
       expect(keys).toEqual(defaultKeys);
     });
-});
-
-test("Number of line should be same among all locals", async () => {
-  const defaultData = await readFile(path.join(__dirname, "locals", "en.js"));
-  const defaultLoc = defaultData.split("\n").length;
-
-  const promises = languages
-    .filter(lang => lang !== "en")
-    .map(lang => path.join(__dirname, "locals", `${lang}.js`))
-    .map(file => readFile(file));
-
-  const results = await Promise.all(promises);
-
-  results.map(res => res.split("\n").length).forEach(loc => {
-    expect(loc).toEqual(defaultLoc);
-  });
 });
