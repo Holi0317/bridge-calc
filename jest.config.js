4b58a5ae913e89e27f63715dca27af497bfc4d68
Hollis Wu <holliswuhollis@gmail.com>
2019-05-12 00:29:44 +0800

    Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,14 @@
+const { jsWithTs } = require("ts-jest/presets");
+
 module.exports = {
   transform: {
-    ".(js|ts|tsx)": "ts-jest"
+    ...jsWithTs.transform
   },
-  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
   transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash-es)"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   watchPathIgnorePatterns: ["<rootDir>/node_modules/"],
   globals: {
     "ts-jest": {
-      tsConfigFile: "tsconfig.test.json"
+      tsConfig: "tsconfig.test.json"
     }
-  },
-  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
-  coverageReporters: ["text-summary", "html"]
+  }
 };
