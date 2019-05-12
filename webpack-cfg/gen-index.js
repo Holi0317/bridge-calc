efc5de1cd7b5d621d6e22626de4cf9ff55964221
Hollis Wu <holliswuhollis@gmail.com>
2019-05-13 01:34:22 +0800

    Remove minify JS code in generating index.html

--- webpack-cfg/gen-index.js
+++ webpack-cfg/gen-index.js
@@ -1,14 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { transform } = require("@babel/core");
 const { ENV } = require("./paths");
-
-const babelOpt = {
-  babelrc: false,
-  comments: false,
-  filename: "index.html",
-  minified: true,
-  presets: ["minify"]
-};
 
 const htmlSettings = {
   template: "assets/index.ejs",
@@ -17,10 +8,7 @@ const htmlSettings = {
     ENV === "production"
       ? {
           removeComments: true,
-          collapseWhitespace: true,
-          minifyJS(code) {
-            return transform(code, babelOpt).code;
-          }
+          collapseWhitespace: true
         }
       : undefined
 };
