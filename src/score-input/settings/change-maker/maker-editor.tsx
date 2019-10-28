74400268a802e22c37e7529a03814347b26cf9bd
Hollis Wu <holliswuhollis@gmail.com>
2019-10-28 22:47:07 +0800

    Use hooks in maker-editor

--- src/score-input/settings/change-maker/maker-editor.tsx
+++ src/score-input/settings/change-maker/maker-editor.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { bindActionCreators } from "redux";
-import flowRight from "lodash-es/flowRight";
-import { WithTranslation, withTranslation } from "react-i18next";
-import { connect } from "react-redux";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 import Button from "@material-ui/core/Button";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
@@ -16,80 +14,56 @@ import { changePlayersAction } from "../../actions/change-players";
 import { initSettingsAction } from "../actions/init-settings";
 import { setMakerAction } from "../actions/set-maker";
 import { showToastAction } from "../../../toast-singleton/actions/show-toast";
-import { RootState, Dispatch } from "../../../types";
+import { RootState } from "../../../types";
+import { useAction } from "../../../hooks/use-action";
 
-const mapStateToProps = (state: RootState) => ({
-  names: namesSelector(state),
-  namesSource: makerSourceSelector(state),
-  rounds: roundsSelector(state),
-  maker: makerSelector(state),
-  disabled: namesChangedSelector(state),
-  currentGame: state.currentGame
-});
+export function MakerEditor() {
+  const { t } = useTranslation();
 
-const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators(
-    {
-      changePlayers: changePlayersAction,
-      init: initSettingsAction,
-      setMaker: setMakerAction,
-      showToast: showToastAction
-    },
-    dispatch
-  );
+  const names = useSelector(namesSelector);
+  const namesSource = useSelector(makerSourceSelector);
+  const rounds = useSelector(roundsSelector);
+  const maker = useSelector(makerSelector);
+  const disabled = useSelector(namesChangedSelector);
+  const currentGame = useSelector((state: RootState) => state.currentGame);
 
-type stateType = ReturnType<typeof mapStateToProps>;
-type dispatchType = ReturnType<typeof mapDispatchToProps>;
+  const changePlayers = useAction(changePlayersAction);
+  const init = useAction(initSettingsAction);
+  const setMaker = useAction(setMakerAction);
+  const showToast = useAction(showToastAction);
 
-type MakerEditorProps = stateType & dispatchType & WithTranslation;
-
-export class MakerEditorImpl extends React.Component<MakerEditorProps> {
-  public render() {
-    const { maker, namesSource, disabled, setMaker, t } = this.props;
-
-    return (
-      <>
-        <ExpansionPanelDetails>
-          <Dropdown
-            label={t("Maker")}
-            disabled={disabled}
-            source={disabled ? [] : namesSource}
-            value={disabled ? "" : maker || ""}
-            onChange={setMaker}
-          />
-        </ExpansionPanelDetails>
-
-        <ExpansionPanelActions>
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={disabled}
-            onClick={this.commit}
-          >
-            {t("Change maker")}
-          </Button>
-        </ExpansionPanelActions>
-      </>
-    );
-  }
-
-  private commit = () => {
-    const { names, rounds, maker, changePlayers, showToast, t } = this.props;
+  const commit = () => {
     changePlayers(names, maker!, rounds!);
     showToast(t("Maker changed!"));
 
     // Reset game settings in next tick
     window.setTimeout(() => {
-      const { currentGame, init } = this.props;
       init(currentGame);
     }, 0);
   };
-}
 
-export const MakerEditor = flowRight(
-  withTranslation(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(MakerEditorImpl);
+  return (
+    <>
+      <ExpansionPanelDetails>
+        <Dropdown
+          label={t("Maker")}
+          disabled={disabled}
+          source={disabled ? [] : namesSource}
+          value={disabled ? "" : maker || ""}
+          onChange={setMaker}
+        />
+      </ExpansionPanelDetails>
+
+      <ExpansionPanelActions>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={disabled}
+          onClick={commit}
+        >
+          {t("Change maker")}
+        </Button>
+      </ExpansionPanelActions>
+    </>
+  );
+}
