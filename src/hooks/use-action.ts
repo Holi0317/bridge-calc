9339965aee87d7172cd0e85ccf1b478253409cec
Hollis Wu <holliswuhollis@gmail.com>
2019-10-28 22:15:01 +0800

    Add useAction hook

---
+++ src/hooks/use-action.ts
@@ -0,0 +1,25 @@
+import { Actions } from "../types";
+import { useDispatch } from "react-redux";
+import { useCallback } from "react";
+
+/**
+ * React hook for creating redux action.
+ *
+ * Accepts a action creator and returns a function that will call
+ * the action creator.
+ *
+ * Note that the return type of the returned function is not accurate.
+ * The return type should be unknown.
+ */
+export function useAction<F extends (...args: any[]) => Actions>(
+  creator: F
+): F {
+  const dispatch = useDispatch();
+  return useCallback(
+    (...args: any[]) => {
+      const action = creator(args);
+      dispatch(action);
+    },
+    [dispatch]
+  ) as F;
+}
