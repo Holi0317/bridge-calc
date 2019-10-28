4d6dae98d3cdd438db3c78bd3430b2b800455541
Hollis Wu <holliswuhollis@gmail.com>
2019-10-29 03:11:16 +0800

    Use hooks in layout

--- src/score-input/layout.tsx
+++ src/score-input/layout.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import { Route, withRouter, matchPath } from "react-router-dom";
-import flowRight from "lodash-es/flowRight";
-import { WithTranslation, withTranslation } from "react-i18next";
+import { Route, matchPath } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import { routes } from "./routes";
-import { RouteComponentProps } from "react-router";
+import { useLocation, useHistory } from "react-router";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import Paper from "@material-ui/core/Paper";
 import { ErrorBoundary } from "../error-boundary";
+import { useState, useEffect, useCallback } from "react";
 
 function getActive(pathname: string): number {
   /*
@@ -18,74 +18,70 @@ function getActive(pathname: string): number {
   return index === -1 ? 0 : index;
 }
 
-type LayoutProps = RouteComponentProps<any> & WithTranslation;
-interface LayoutState {
-  active: number;
+/**
+ * React hook for current active tab index.
+ *
+ * This hook will react to route change and update accordingly.
+ *
+ * The returns value is the currently activated index.
+ */
+function useTab(): number {
+  const [active, setActive] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    setActive(getActive(location.pathname));
+  });
+
+  const newActive = getActive(location.pathname);
+  if (newActive != active) {
+    setActive(newActive);
+  }
+
+  return active;
 }
 
-export class LayoutImpl extends React.Component<LayoutProps> {
-  public state: LayoutState = {
-    active: 0
-  };
+export function Layout() {
+  const { t } = useTranslation();
+  const history = useHistory();
 
-  public componentWillMount() {
-    this.setActive(this.props.location.pathname);
-  }
+  const active = useTab();
 
-  public componentWillReceiveProps(nextProps: LayoutProps) {
-    if (nextProps.location !== this.props.location) {
-      this.setActive(nextProps.location.pathname);
-    }
-  }
+  const tabChange = useCallback(
+    (_event: any, routeIndex: number) => {
+      const { path } = routes[routeIndex];
+      if (path != null) {
+        history.push(path);
+      }
+    },
+    [routes, history]
+  );
 
-  public render() {
-    const { t } = this.props;
-    const { active } = this.state;
-
-    return (
-      <div>
-        <Paper>
-          <Tabs
-            value={active}
-            onChange={this.tabChange}
-            indicatorColor="primary"
-            textColor="primary"
-            centered
-          >
-            {routes.map((route, index) => (
-              <Tab key={index} label={t(route.name)} value={index} />
-            ))}
-          </Tabs>
-        </Paper>
-        <ErrorBoundary>
+  return (
+    <div>
+      <Paper>
+        <Tabs
+          value={active}
+          onChange={tabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
           {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              component={route.component}
-              exact={route.exact}
-            />
+            <Tab key={index} label={t(route.name)} value={index} />
           ))}
-        </ErrorBoundary>
-      </div>
-    );
-  }
-
-  private tabChange = (_event: any, routeIndex: number) => {
-    const { path } = routes[routeIndex];
-    if (path != null) {
-      this.props.history.push(path);
-    }
-  };
-
-  private setActive(pathname: string) {
-    this.setState(() => ({
-      active: getActive(pathname)
-    }));
-  }
+        </Tabs>
+      </Paper>
+      <ErrorBoundary>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            component={route.component}
+            exact={route.exact}
+          />
+        ))}
+      </ErrorBoundary>
+    </div>
+  );
 }
-
-export const Layout = flowRight(
-  withRouter,
-  withTranslation()
-)(LayoutImpl);
