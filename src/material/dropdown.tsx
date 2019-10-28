a1e7792bb3396a8fffddb1382a4622de0e8edc77
Hollis Wu <holliswuhollis@gmail.com>
2019-10-29 02:46:21 +0800

    Use hooks in dropdown

--- src/material/dropdown.tsx
+++ src/material/dropdown.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
@@ -23,42 +24,38 @@ export interface DropdownProps<T extends number | string> {
   onChange(value: T): void;
 }
 
-export class Dropdown<
-  SourceType extends number | string
-> extends React.Component<DropdownProps<SourceType>> {
-  private readonly _uid = cuid();
+export function Dropdown<SourceType extends number | string>({
+  source,
+  label,
+  error,
+  onChange,
+  ...rest
+}: DropdownProps<SourceType>) {
+  const [uid] = useState(() => cuid());
 
-  public render() {
-    const { source, label, error, ...rest } = this.props;
-
-    return (
-      <FormControl error={error != null} margin="normal">
-        <InputLabel htmlFor={this._uid}>{label}</InputLabel>
-        <Select
-          {...rest}
-          inputProps={{ name: label, id: this._uid }}
-          classes={{
-            root: classes.dropdownRoot,
-            selectMenu: classes.dropdownSelectMenu
-          }}
-          onChange={this.handleChange}
-        >
-          {source.map(item => (
-            <MenuItem
-              key={item.label}
-              value={item.value}
-              disabled={item.disabled}
-            >
-              {item.label}
-            </MenuItem>
-          ))}
-        </Select>
-        {error != null && <FormHelperText>{error}</FormHelperText>}
-      </FormControl>
-    );
-  }
-
-  private handleChange = (event: any) => {
-    this.props.onChange(event.target.value);
-  };
+  return (
+    <FormControl error={error != null} margin="normal">
+      <InputLabel htmlFor={uid}>{label}</InputLabel>
+      <Select
+        {...rest}
+        inputProps={{ name: label, id: uid }}
+        classes={{
+          root: classes.dropdownRoot,
+          selectMenu: classes.dropdownSelectMenu
+        }}
+        onChange={(event: any) => onChange(event.target.value)}
+      >
+        {source.map(item => (
+          <MenuItem
+            key={item.label}
+            value={item.value}
+            disabled={item.disabled}
+          >
+            {item.label}
+          </MenuItem>
+        ))}
+      </Select>
+      {error != null && <FormHelperText>{error}</FormHelperText>}
+    </FormControl>
+  );
 }
