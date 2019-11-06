430169144d7ce57d27227410fe0d139d6666d1b9
Hollis Wu <holliswuhollis@gmail.com>
2019-11-07 00:44:33 +0800

    Use hooks for skip rounds

--- src/score-input/settings/round-management/skip-rounds.tsx
+++ src/score-input/settings/round-management/skip-rounds.tsx
@@ -1,85 +1,41 @@
 import * as React from "react";
-import flowRight from "lodash-es/flowRight";
-import { WithTranslation, withTranslation } from "react-i18next";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 import Button from "@material-ui/core/Button";
 import { skipAction } from "../../actions/skip";
 import { initSettingsAction } from "../actions/init-settings";
 import { showToastAction } from "../../../toast-singleton/actions/show-toast";
 import { remainingRoundsSelector } from "../../selectors/remaining-rounds";
 import { currentRoundSelector } from "../../selectors/current-round";
-import { RootState, Dispatch } from "../../../types";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import classes from "../settings.pcss";
+import { useAction } from "../../../hooks/use-action";
+import { useCallback } from "react";
+import { currentGameSelector } from "../../selectors/current-game";
 
-const mapStateToProps = (state: RootState) => ({
-  currentGame: state.currentGame,
-  currentRound: currentRoundSelector(state),
-  remainingRounds: remainingRoundsSelector(state)
-});
+export function SkipRounds() {
+  const { t } = useTranslation();
 
-const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators(
-    {
-      skip: skipAction,
-      init: initSettingsAction,
-      showToast: showToastAction
-    },
-    dispatch
-  );
+  const currentGame = useSelector(currentGameSelector);
+  const currentRound = useSelector(currentRoundSelector);
+  const remainingRounds = useSelector(remainingRoundsSelector);
 
-type stateType = ReturnType<typeof mapStateToProps>;
-type dispatchType = ReturnType<typeof mapDispatchToProps>;
+  const skipAct = useAction(skipAction);
+  const init = useAction(initSettingsAction);
+  const showToast = useAction(showToastAction);
 
-type SkipRoundsProps = stateType & dispatchType & WithTranslation;
-
-export class SkipRoundsImpl extends React.Component<SkipRoundsProps> {
-  public render() {
-    const { remainingRounds, t } = this.props;
-    return (
-      <ExpansionPanelDetails>
-        <Button
-          variant="outlined"
-          className={classes.skipRoundsBtn}
-          onClick={this.skip(1)}
-        >
-          {t("Skip this round")}
-        </Button>
-        <Button
-          variant="outlined"
-          className={classes.skipRoundsBtn}
-          disabled={remainingRounds <= 1}
-          onClick={this.skip(remainingRounds - 1)}
-        >
-          {t("Skip to last round")}
-        </Button>
-        <Button
-          variant="outlined"
-          className={classes.skipRoundsBtn}
-          onClick={this.skip(remainingRounds)}
-        >
-          {t("End game")}
-        </Button>
-      </ExpansionPanelDetails>
-    );
-  }
-
-  private skip = (n: number) => {
-    return () => {
+  const skip = useCallback(
+    (n: number) => {
       if (n <= 0) {
-        const { showToast, t } = this.props;
         showToast(t("Already at the last round. Cannot skip."));
         return;
       }
 
-      const { remainingRounds, skip } = this.props;
       const isEndGame = remainingRounds <= n;
-      skip(n);
+      skipAct(n);
 
       if (!isEndGame) {
         window.setTimeout(() => {
-          const { init, currentGame, currentRound, showToast, t } = this.props;
           init(currentGame);
 
           if (currentRound !== null) {
@@ -91,14 +47,34 @@ export class SkipRoundsImpl extends React.Component<SkipRoundsProps> {
           }
         }, 0);
       }
-    };
-  };
-}
+    },
+    [skipAct, remainingRounds, showToast, t]
+  );
 
-export const SkipRounds = flowRight(
-  withTranslation(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(SkipRoundsImpl);
+  return (
+    <ExpansionPanelDetails>
+      <Button
+        variant="outlined"
+        className={classes.skipRoundsBtn}
+        onClick={() => skip(1)}
+      >
+        {t("Skip this round")}
+      </Button>
+      <Button
+        variant="outlined"
+        className={classes.skipRoundsBtn}
+        disabled={remainingRounds <= 1}
+        onClick={() => skip(remainingRounds - 1)}
+      >
+        {t("Skip to last round")}
+      </Button>
+      <Button
+        variant="outlined"
+        className={classes.skipRoundsBtn}
+        onClick={() => skip(remainingRounds)}
+      >
+        {t("End game")}
+      </Button>
+    </ExpansionPanelDetails>
+  );
+}
