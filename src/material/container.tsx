d3b511161b3914416327923c1344971c3a60d6c6
Hollis Wu <holliswuhollis@gmail.com>
2019-11-19 01:12:32 +0800

    Use styled-components for material container

--- src/material/container.tsx
+++ src/material/container.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import classes from "./container.pcss";
+import styled from "styled-components/macro";
 
-interface ContainerProps {
-  children: React.ReactNode;
-}
+export const Container = styled.div`
+  flex: 1 1 100%;
+  padding-left: 16px;
+  padding-right: 16px;
+  margin: 0 auto 100px;
 
-export function Container({ children }: ContainerProps) {
-  return <div className={classes.container}>{children}</div>;
-}
+  ${props => props.theme.breakpoints.up("sm")} {
+    max-width: 750px;
+  }
+
+  ${props => props.theme.breakpoints.up("lg")} {
+    max-width: 960px;
+  }
+
+  ${props => props.theme.breakpoints.up("xl")} {
+    max-width: 1140px;
+  }
+`;
