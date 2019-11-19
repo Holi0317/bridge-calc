269258e6501442ef2b8d4010c5822b42224bd9ab
Hollis Wu <holliswuhollis@gmail.com>
2019-11-19 22:46:28 +0800

    Do not hard code link color

--- src/global-settings/version-display.tsx
+++ src/global-settings/version-display.tsx
@@ -1,28 +1,40 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Typography from "@material-ui/core/Typography";
-import classes from "./version-display.pcss";
+import styled from "styled-components/macro";
+
+const Container = styled.div`
+  padding-top: 0.5em;
+`;
+
+const StyledLink = styled.a`
+  color: ${props => props.theme.palette.primary.light};
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export function VersionDisplay() {
   const { t } = useTranslation();
 
-  // FIXME: version should be version-gitHASH
   const version = `${process.env.REACT_APP_VERSION}-${process.env.REACT_APP_GIT_SHA}`;
 
   return (
-    <div className={classes.text}>
+    <Container>
       <Typography variant="caption">
         {t("Version: {{version}}", { version })}
       </Typography>
       <Typography variant="body1">
-        <a
+        <StyledLink
           href="https://gitlab.com/holi0317/bridge-calc/tags"
           rel="noopener noreferrer"
           target="_blank"
         >
           {t("See Changelog")}
-        </a>
+        </StyledLink>
       </Typography>
-    </div>
+    </Container>
   );
 }
