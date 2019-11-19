98b42c008aea91127397374146c394d9cf0c8409
Hollis Wu <holliswuhollis@gmail.com>
2019-11-19 22:36:15 +0800

    Use styled-components for not-found

--- src/not-found/not-found.tsx
+++ src/not-found/not-found.tsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import styled from "styled-components/macro";
 import AlertError from "@material-ui/icons/Error";
-import classes from "./not-found.pcss";
+
+const Container = styled.div`
+  height: 100%;
+  margin-top: 1em;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const StyledIcon = styled(AlertError)`
+  width: 8em !important;
+  height: 8em !important;
+  margin-bottom: 1em !important;
+  fill: ${props => props.theme.palette.text.primary} !important;
+`;
+
+const TextContainer = styled.div`
+  font-size: 22px;
+  color: ${props => props.theme.palette.text.primary};
+`;
+
+const StyledLink = styled(Link)`
+  color: #428bca;
+`;
 
 export function NotFound() {
   const { t } = useTranslation();
 
   return (
-    <div className={classes.container}>
-      <AlertError className={classes.icon} />
-      <div className={classes.text}>{t("Page not found!")}</div>
-      <Link className={classes.link} to="/">
-        {t("Click here to return to main menu")}
-      </Link>
-    </div>
+    <Container>
+      <StyledIcon />
+      <TextContainer>{t("Page not found!")}</TextContainer>
+      <StyledLink to="/">{t("Click here to return to main menu")}</StyledLink>
+    </Container>
   );
 }
