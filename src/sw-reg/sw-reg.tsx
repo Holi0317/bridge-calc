e40ec032aa09f0654e26797d3728f4bfb7e2b256
Hollis Wu <holliswuhollis@gmail.com>
2019-11-20 12:13:35 +0800

    Fix registration of service worker

--- src/sw-reg/sw-reg.tsx
+++ src/sw-reg/sw-reg.tsx
@@ -1,79 +1,23 @@
-import React from "react";
-import flowRight from "lodash-es/flowRight";
-import { connect } from "react-redux";
-import { WithTranslation, withTranslation } from "react-i18next";
-import { bindActionCreators } from "redux";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { showToastAction } from "../toast-singleton/actions/show-toast";
-import { Dispatch } from "../types";
+import { useAction } from "../hooks/use-action";
+import { register } from "./serviceWorker";
 
-const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators(
-    {
-      showToast: showToastAction
-    },
-    dispatch
-  );
+export function SWReg() {
+  const { t } = useTranslation();
+  const showToast = useAction(showToastAction);
 
-type dispatchType = ReturnType<typeof mapDispatchToProps>;
-
-type SWRegProps = dispatchType & WithTranslation;
-
-class SWRegImpl extends React.Component<SWRegProps> {
-  public componentDidMount() {
-    if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-      this.install();
-    }
-  }
-
-  public render() {
-    return null;
-  }
-
-  private async install() {
-    try {
-      const scriptURL = (document.baseURI || "/") + "sw.js";
-      const registration = await navigator.serviceWorker.register(scriptURL);
-      if (!registration) {
-        // WTF no registration????
-        return;
+  useEffect(() => {
+    register({
+      onUpdate() {
+        showToast(t("App will update after page reload"));
+      },
+      onSuccess() {
+        showToast(t("This app is now available offline"));
       }
+    });
+  }, [showToast, t]);
 
-      // New installation notify logic
-      const sw = registration.installing || registration.waiting;
-      if (sw) {
-        sw.addEventListener("statechange", () => {
-          if (sw.state === "activated") {
-            this.showToast("This app is now available offline");
-          }
-        });
-      }
-
-      // Update notification logic
-      registration.addEventListener("updatefound", () => {
-        const newWorker = registration.installing;
-        if (newWorker) {
-          newWorker.addEventListener("statechange", () => {
-            if (newWorker.state === "installed") {
-              this.showToast("App will update after page reload");
-            }
-          });
-        }
-      });
-    } catch (err) {
-      console.error("Error when registering service worker", err);
-    }
-  }
-
-  private showToast = (msg: string) => {
-    const { showToast, t } = this.props;
-    showToast(t(msg));
-  };
+  return null;
 }
-
-export const SWReg = flowRight(
-  withTranslation(),
-  connect(
-    null,
-    mapDispatchToProps
-  )
-)(SWRegImpl);
