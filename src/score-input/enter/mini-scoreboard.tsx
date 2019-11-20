a8415c8aa0009d922f5912d4a967ffeca63b0c21
Hollis Wu <holliswuhollis@gmail.com>
2019-11-20 19:56:44 +0800

    Use hooks for mini scoreboard

--- src/score-input/enter/mini-scoreboard.tsx
+++ src/score-input/enter/mini-scoreboard.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { connect } from "react-redux";
-import { RootState } from "../../types";
+import { useSelector } from "react-redux";
 import { ScoreboardTable } from "../scoreboard/scoreboard-table";
+import { currentGameSelector } from "../selectors/current-game";
 
-const mapStateToProps = (state: RootState) => ({
-  entry: state.currentGame
-});
+export function MiniScoreboard() {
+  const entry = useSelector(currentGameSelector);
 
-type stateType = ReturnType<typeof mapStateToProps>;
-
-export function MiniScoreboardImpl({ entry }: stateType) {
   if (entry == null) {
     return null;
   }
   return <ScoreboardTable entry={entry} mini />;
 }
-
-export const MiniScoreboard = connect(mapStateToProps)(MiniScoreboardImpl);
